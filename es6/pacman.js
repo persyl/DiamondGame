7bb66cb3fd3f53297c333d68f722269361805c5f
per.lundkvist <per.lundkvist@di.se>
2015-12-05 09:19:47 +0100

    Moved logic into PacMan class

--- es6/pacman.js
+++ es6/pacman.js
@@ -1,15 +1,28 @@
 class PacMan {
 
-  constructor() {
-
+  constructor(doc) {
+    this.doc = doc;
   }
 
   init() {
-    return 'Pac Man game is initialized!';
+    const body = this.doc.querySelector('body');
+    const divElement = this.doc.createElement('div');
+    const h1Element = this.doc.createElement('h1');
+    h1Element.innerText = this;
+    // const h1Text = this.doc.createTextNode(DiPacMan);
+    // h1Element.appendChild(h1Text);
+    divElement.appendChild(h1Element);
+
+
+    const pElement = this.doc.createElement('p');
+    pElement.innerHTML = 'Game initialized!';
+    divElement.appendChild(pElement);
+
+    this.doc.body.appendChild(divElement);
   }
 
   toString() {
-    return 'Pac Man! ';
+    return 'Pac Man!';
   }
 }
 
